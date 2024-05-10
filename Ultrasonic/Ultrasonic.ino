5ff0e7a2e9d6577f0a17fa2d0757f55992ac4961
rockyyy1 <124854700+rockyyy1@users.noreply.github.com>
2024-05-10 12:29:42 +0800

    Add files via upload

--- Ultrasonic/Ultrasonic.ino
+++ Ultrasonic/Ultrasonic.ino
@@ -1,8 +1,80 @@
+//Configuration
+#include "config.h"
+#include <WiFi.h>
 
+//Button Pins
 #define TRIG_PIN 26 
 #define ECHO_PIN 25
 
+//constants
 float duration_us, distance_cm;
+const char* ssid     = "Rocky's iPhone";
+const char* password = "ycrx8370";
+
+
+// set up the 'ultrasonic' feed
+AdafruitIO_Feed *distance = io.feed("ultrasonic");
+
+void SetupWifi(){
+  Serial.begin(9600);
+  delay(10);
+
+  Serial.println();
+  Serial.print("[WiFi] Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+// Auto reconnect is set true as default
+// To set auto connect off, use the following function
+//    WiFi.setAutoReconnect(false);
+
+  // Will try for about 10 seconds (20x 500ms)
+  int tryDelay = 500;
+  int numberOfTries = 20;
+
+  // Wait for the WiFi event
+  while (true) {
+      
+      switch(WiFi.status()) {
+        case WL_NO_SSID_AVAIL:
+          Serial.println("[WiFi] SSID not found");
+          break;
+        case WL_CONNECT_FAILED:
+          Serial.print("[WiFi] Failed - WiFi not connected! Reason: ");
+          return;
+          break;
+        case WL_CONNECTION_LOST:
+          Serial.println("[WiFi] Connection was lost");
+          break;
+        case WL_SCAN_COMPLETED:
+          Serial.println("[WiFi] Scan is completed");
+          break;
+        case WL_DISCONNECTED:
+          Serial.println("[WiFi] WiFi is disconnected");
+          break;
+        case WL_CONNECTED:
+          Serial.println("[WiFi] WiFi is connected! to ");
+          Serial.print("[WiFi] IP address: ");
+          Serial.println(WiFi.localIP());
+          return;
+          break;
+        default:
+          Serial.print("[WiFi] WiFi Status: ");
+          Serial.println(WiFi.status());
+          break;
+      }
+      delay(tryDelay);
+      
+      if(numberOfTries <= 0){
+        Serial.print("[WiFi] Failed to connect to WiFi!");
+        // Use disconnect function to force stop trying to connect
+        WiFi.disconnect();
+        return;
+      } else {
+        numberOfTries--;
+      }
+  }
+}
 
 void setup() {
   Serial.begin (9600);
@@ -11,9 +83,27 @@ void setup() {
   pinMode(TRIG_PIN, OUTPUT);
   // configure the echo pin to input mode
   pinMode(ECHO_PIN, INPUT);
+
+  //connect to wifi
+  SetupWifi();
+
+  // connect to io.adafruit.com
+  Serial.print("Connecting to Adafruit IO");
+  io.connect();
+  //wait for connection
+  while(io.status() < AIO_CONNECTED) {
+  Serial.print(".");
+  delay(500);
+  }
+  // we are connected
+  Serial.println();
+  Serial.println(io.statusText());
 }
 
+
 void loop() {
+  io.run();
+
   // generate 10-microsecond pulse to TRIG pin
   digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
@@ -29,6 +119,8 @@ void loop() {
   Serial.print("distance: ");
   Serial.print(distance_cm);
   Serial.println(" cm");
+  distance->save(distance_cm);
 
-  delay(500);
+
+  delay(5000);
 }
---
+++ Ultrasonic/config.h
@@ -0,0 +1,39 @@
+/************************ Adafruit IO Config *******************************/
+
+// visit io.adafruit.com if you need to create an account,
+// or if you need your Adafruit IO key.
+#define IO_USERNAME "RESETJ"
+#define IO_KEY "aio_dxBb34M5po3QFEazwQeiRAJqvVTd"
+
+/******************************* WIFI **************************************/
+
+//#define WIFI_SSID "IOT_2.4G"
+//#define WIFI_PASS "107WirelessNetwork"
+#define WIFI_SSID "Rocky's iPhone"
+#define WIFI_PASS "ycrx8370"
+
+// uncomment the following line if you are using airlift
+//#define USE_AIRLIFT
+
+// uncomment the following line if you are using winc1500
+// #define USE_WINC1500
+
+// comment out the following lines if you are using fona or ethernet
+#include "AdafruitIO_WiFi.h"
+
+#if defined(USE_AIRLIFT) || defined(ADAFRUIT_METRO_M4_AIRLIFT_LITE) ||         \
+    defined(ADAFRUIT_PYPORTAL)
+// Configure the pins used for the ESP32 connection
+#if !defined(SPIWIFI_SS) // if the wifi definition isnt in the board variant
+// Don't change the names of these #define's! they match the variant ones
+#define SPIWIFI SPI
+#define SPIWIFI_SS 10  // Chip select pin
+#define SPIWIFI_ACK 9  // a.k.a BUSY or READY pin
+#define ESP32_RESETN 6 // Reset pin
+#define ESP32_GPIO0 -1 // Not connected
+#endif
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS, SPIWIFI_SS,
+                   SPIWIFI_ACK, ESP32_RESETN, ESP32_GPIO0, &SPIWIFI);
+#else
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
+#endif
